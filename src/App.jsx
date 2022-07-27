410358399e3f83294520dc550888e93148ac0b38
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2022-07-27 11:41:39 -0400

    Ambiente criado com React em JavaScript

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//Importação de estilização
+import './global.css'
+import styles from './App.module.css'
+
+//Importação de bibliotecas
+import React from 'react'
+
+//Importação de componentes
+import { Header } from './components/Header.jsx'
+
+export function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+//Importação de estilização
+import '../global.css'
+import styles from './Header.module.css'
+
+//Importação de bibliotecas
+import React from 'react'
+
+//Importação de componentes e ícones
+import rocket from '../assets/rocket.svg'
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={rocket} alt="Logo do TodoList" />
+      <h1>
+        to<span>do</span>
+      </h1>
+    </header>
+  )
+}
