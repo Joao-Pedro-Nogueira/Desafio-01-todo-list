25fbe80200cebe8c36ec8696df1ccac31a372c3a
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2022-08-02 20:16:38 -0400

    Header estilizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 //Importação de estilização
 import './global.css'
 import styles from './App.module.css'
+import { Header } from './components/Header'
 
 function App() {
   return (
     <div>
-     
+      <Header />
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,21 @@
 import '../global.css'
 import styles from './Header.module.css'
 
+//Importação de bibliotecas
+import React from 'react'
+
+//Importação de componentes e ícones
+import rocket from '../assets/rocket.svg'
+
 export function Header() {
-  return(
-    <div>
-      <h1>Header</h1>
-    </div>
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContainer}>
+        <img src={rocket} alt="Logo do TodoList" />
+        <h1>
+          to<span>do</span>
+        </h1>
+      </div>
+    </header>
   )
 }
