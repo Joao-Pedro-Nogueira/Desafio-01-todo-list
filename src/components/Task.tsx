0f9765863016b7f45d674622b26d0dc70fdcc24f
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2022-08-02 20:32:12 -0400

    Front-end estático básico pronto

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,10 +1,20 @@
+//Importação de estilização
 import '../global.css'
 import styles from './Task.module.css'
 
+//Importação de bibliotecas
+import { Trash } from 'phosphor-react'
+
 export function Task() {
-  return(
-    <div>
-      <h1>Header</h1>
+  return (
+    <div className={styles.task}>
+      <button className={styles.checkContainer}>
+        <div />
+      </button>
+      <p>Ler Institutas da Fé Cristã de João Calvino</p>
+      <button className={styles.deleteButton}>
+        <Trash size={24} />
+      </button>
     </div>
   )
 }
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -2,10 +2,34 @@
 import '../global.css'
 import styles from './Tasks.module.css'
 
+//Importação de bibliotecas
+import { PlusCircle } from 'phosphor-react'
+import { Task } from './Task'
+
 export function Tasks() {
-  return(
-    <div>
-      <h1>Tasks</h1>
+  return (
+    <div className={styles.container}>
+      <form className={styles.taskForm}>
+        <textarea placeholder="Adicione uma nova tarefa"></textarea>
+        <button>
+          <strong>Criar</strong>
+          <PlusCircle size={16} />
+        </button>
+      </form>
+      {/* Container de tasks */}
+      <div className={styles.wrapper}>
+        <header className={styles.header}>
+          <div>
+            <p>Tarefas criadas</p>
+            <span>5</span>
+          </div>
+          <div>
+            <p className={styles.purpleText}>Concluídas</p>
+            <span>2 de 5</span>
+          </div>
+        </header>
+        < Task />
+      </div>
     </div>
   )
 }
